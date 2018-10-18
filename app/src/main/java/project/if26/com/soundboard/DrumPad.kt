6ad8746246866fc4275a6deacbbba4da309bfbd9
Allan Elleuch <allan.elleuch@gmail.com>
2018-10-18 15:27:51 +0200

    Add  LockScreen and DrumPad activity

---
+++ app/src/main/java/project/if26/com/soundboard/DrumPad.kt
@@ -0,0 +1,12 @@
+package project.if26.com.soundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DrumPad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drum_pad)
+    }
+}
---
+++ app/src/main/java/project/if26/com/soundboard/LockScreen.kt
@@ -0,0 +1,12 @@
+package project.if26.com.soundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LockScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lock_screen)
+    }
+}
