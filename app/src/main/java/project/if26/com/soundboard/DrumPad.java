878625e8cd350ba0e8c656b82c94a4ef5a273418
Allan Elleuch <allan.elleuch@gmail.com>
2018-10-19 15:46:47 +0200

    Drumpad change to java

---
+++ app/src/main/java/project/if26/com/soundboard/DrumPad.java
@@ -0,0 +1,13 @@
+package project.if26.com.soundboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrumPad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drum_pad);
+    }
+}
