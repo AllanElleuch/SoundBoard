9ec627b7ddcf616ae8e24a47b79adcaae2787671
Marshlyin <flavien.belli@utt.fr>
2018-11-04 12:32:03 +0100

    Amélioration son + ajout titre et police "lofi"

--- app/src/main/java/project/if26/com/soundboard/DrumPad.java
+++ app/src/main/java/project/if26/com/soundboard/DrumPad.java
@@ -54,52 +54,52 @@ public class DrumPad extends AppCompatActivity {
     }
 
     public void playsound1 (View v) {
-        sp.play(sound1,1.0f,1.0f,0,0,10f);
+        sp.play(sound1,1.0f,1.0f,0,0,1);
     }
     public void playsound2 (View v) {
-        sp.play(sound2,1.0f,1.0f,0,0,10f);
+        sp.play(sound2,1.0f,1.0f,0,0,1);
 
     }
     public void playsound3 (View v) {
-        sp.play(sound3,1.0f,1.0f,0,0,10f);
+        sp.play(sound3,1.0f,1.0f,0,0,1);
 
     }
     public void playsound4 (View v) {
-        sp.play(sound4,1.0f,1.0f,0,0,10f);
+        sp.play(sound4,1.0f,1.0f,0,0,1);
 
     }
     public void playsound5 (View v) {
-        sp.play(sound5,1.0f,1.0f,0,0,10f);
+        sp.play(sound5,1.0f,1.0f,0,0,1);
 
     }
     public void playsound6 (View v) {
-        sp.play(sound6,1.0f,1.0f,0,0,10f);
+        sp.play(sound6,1.0f,1.0f,0,0,1);
 
     }
     public void playsound7 (View v) {
-        sp.play(sound7,1.0f,1.0f,0,0,10f);
+        sp.play(sound7,1.0f,1.0f,0,0,1f);
 
     }
     public void playsound8 (View v) {
-        sp.play(sound8,1.0f,1.0f,0,0,10f);
+        sp.play(sound8,1.0f,1.0f,0,0,1);
 
     }
     public void playsound9 (View v) {
-        sp.play(sound9,1.0f,1.0f,0,0,10f);
+        sp.play(sound9,1.0f,1.0f,0,0,1);
 
     }
     public void playsound10 (View v) {
-        sp.play(sound10,1.0f,1.0f,0,0,10f);
+        sp.play(sound10,1.0f,1.0f,0,0,1);
 
     }
 
     public void playsound11 (View v) {
-        sp.play(sound11,1.0f,1.0f,0,0,10f);
+        sp.play(sound11,1.0f,1.0f,0,0,1);
 
     }
 
     public void playsound12 (View v) {
-        sp.play(sound12,1.0f,1.0f,0,0,10f);
+        sp.play(sound12,1.0f,1.0f,0,0,1);
 
     }
 
