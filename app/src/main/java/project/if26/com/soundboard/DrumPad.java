ea2e2620c7f39cdb394e4ba349f328eb6563e95d
Allan Elleuch <allan.elleuch@gmail.com>
2018-12-08 19:58:57 +0100

    add code to record and playback a play. Add a button save that need to be coded with database call.

--- app/src/main/java/project/if26/com/soundboard/DrumPad.java
+++ app/src/main/java/project/if26/com/soundboard/DrumPad.java
@@ -6,7 +6,18 @@ import android.os.Bundle;
 
 
 import android.media.SoundPool;
+import android.util.Log;
 import android.view.View;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import kotlin.Pair;
 
 public class DrumPad extends Template_Activity {
 
@@ -24,6 +35,35 @@ public class DrumPad extends Template_Activity {
     private int sound10;
     private int sound11;
     private int sound12;
+
+    private long chrono= System.currentTimeMillis();
+
+    private ArrayList< Pair<String,Long> > listEvenement = new ArrayList< Pair<String,Long> >();
+
+
+    public final void Go_Chrono() {
+        this.chrono = System.currentTimeMillis();
+    }
+
+    public final void addEvent_Chrono(String functionName) {
+        Long chrono2 = System.currentTimeMillis();
+        Long temps = chrono2 - this.chrono;
+        this.chrono += temps;
+
+        Pair tuple = new Pair(functionName, temps);
+        this.listEvenement.add(tuple);
+        Log.v("Chrono", "Temps evenement = " + temps + " ms");
+        String printlist = "";
+
+        String print = "";
+        for(Pair pair : this.listEvenement) {
+            print +=  pair.getSecond().toString() + " || " + pair.getFirst();
+        }
+
+        Log.v("Chrono", "Total evenement = " + print);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,53 +94,157 @@ public class DrumPad extends Template_Activity {
 
     }
 
+    public void record (View v) {
+        this.chrono =  System.currentTimeMillis();
+        this.listEvenement = new ArrayList< Pair<String,Long> >();
+
+    }
+
+    public void save (View v) {
+
+
+    }
+    public void play (View v) {
+
+        Method method;
+
+        for(Pair<String,Long> pair : this.listEvenement){
+
+            Log.v("Chrono", "call : " + pair.getFirst());
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(pair.getSecond());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            playsound(pair.getFirst());
+            /*
+
+
+            try {
+                  method  = this.getClass().getMethod(pair.getFirst(),   View.class);
+
+
+                try {
+                    method.invoke(this, v   );
+                } catch (IllegalArgumentException e) {
+                    throw  new IllegalArgumentException();
+
+                }
+                catch (
+                        IllegalAccessException e) {
+
+                }
+                catch (InvocationTargetException e) {   }
+
+            }
+
+            catch (SecurityException e) {
+                throw  new SecurityException();
+            }
+            catch (NoSuchMethodException e) {
+
+            }
+
+             */
+
+
+        }
+    }
+
+
+    public void playsound(String sound){
+        switch (sound){
+            case "sound1": sp.play(sound1,1.0f,1.0f,0,0,1);
+            break;
+            case "sound2": sp.play(sound2,1.0f,1.0f,0,0,1);
+                break;
+            case "sound3": sp.play(sound3,1.0f,1.0f,0,0,1);
+                break;
+            case "sound4": sp.play(sound4,1.0f,1.0f,0,0,1);
+                break;
+            case "sound5": sp.play(sound5,1.0f,1.0f,0,0,1);
+                break;
+            case "sound6": sp.play(sound6,1.0f,1.0f,0,0,1);
+                break;
+            case "sound7": sp.play(sound7,1.0f,1.0f,0,0,1);
+                break;
+            case "sound8": sp.play(sound8,1.0f,1.0f,0,0,1);
+                break;
+            case "sound9": sp.play(sound9,1.0f,1.0f,0,0,1);
+                break;
+            case "sound10": sp.play(sound10,1.0f,1.0f,0,0,1);
+                break;
+            case "sound11": sp.play(sound11,1.0f,1.0f,0,0,1);
+                break;
+            case "sound12": sp.play(sound12,1.0f,1.0f,0,0,1);
+                break;
+        }
+    }
+
+
     public void playsound1 (View v) {
         sp.play(sound1,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound1");
+
     }
     public void playsound2 (View v) {
         sp.play(sound2,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound2" +
+                "");
 
     }
     public void playsound3 (View v) {
         sp.play(sound3,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound3");
 
     }
     public void playsound4 (View v) {
         sp.play(sound4,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound4");
 
     }
     public void playsound5 (View v) {
         sp.play(sound5,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound5");
 
     }
     public void playsound6 (View v) {
         sp.play(sound6,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound6");
 
     }
     public void playsound7 (View v) {
         sp.play(sound7,1.0f,1.0f,0,0,1f);
+        addEvent_Chrono("sound7");
 
     }
     public void playsound8 (View v) {
         sp.play(sound8,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound8");
 
     }
     public void playsound9 (View v) {
         sp.play(sound9,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound1");
 
     }
     public void playsound10 (View v) {
         sp.play(sound10,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound10");
 
     }
 
     public void playsound11 (View v) {
         sp.play(sound11,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound11");
 
     }
 
     public void playsound12 (View v) {
         sp.play(sound12,1.0f,1.0f,0,0,1);
+        addEvent_Chrono("sound12");
 
     }
 
--- app/src/main/java/project/if26/com/soundboard/MainActivity.kt
+++ app/src/main/java/project/if26/com/soundboard/MainActivity.kt
@@ -21,7 +21,35 @@ class MainActivity : Template_Activity() {
 
 
     private lateinit var button: Button
+    var chrono: Long = 0
+    val listEvenement = ArrayList< Pair<String,Int> >()
 
+
+    fun Go_Chrono() {
+        chrono = java.lang.System.currentTimeMillis()
+    }
+
+    fun addEvent_Chrono() {
+        val chrono2 = java.lang.System.currentTimeMillis()
+        val temps = chrono2 - chrono
+        var tuple =   Pair<String,Int>("",400);
+        listEvenement.add(tuple);
+
+        Log.v("Chrono","Temps evenement = $temps ms")
+        var printlist = "";
+        for (item in listEvenement){
+            printlist += Integer.toString(item.second) + " | ";
+
+
+        }
+    }
+
+    fun Stop_Chrono() {
+        val chrono2 = java.lang.System.currentTimeMillis()
+        val temps = chrono2 - chrono
+        println("Temps ecoule = $temps ms")
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
